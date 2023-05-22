3fc8c8911232b066c69abd90a4dc1d9ba821e3fd
springPotatoes <121741455+springPotatoes@users.noreply.github.com>
2023-05-22 23:56:13 +0900

    Add: Traversal
    Preorder Traversal Functions
    Indoder Traversal functions
    Postorder Traversal functions

---
+++ tree/traversal.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+
+typedef struct btNode {
+	int data;
+	struct btNode* left;
+	struct btNode* right;
+}btTree;
+
+int main(void) {
+	return 0;
+}
+
+
+// Preorder traversal functions
+void preorderTraversal(struct btNode* t) {
+	if (t == NULL) return 0;
+	printf("%c  ", t->data);
+	preorderTraversal(t->left);
+	preorderTraversal(t->right);
+}
+
+// Indoder traversal functions
+void indoderTraversal(struct btNode* t) {
+	if (t == NULL) return;
+	inorderTraversal(t->left);
+	printf("%c  ", t->data);
+	inorderTraersal(t->right);
+}
+
+// Postorder traversal functions
+void postorderTraversal(struct btNode* t) {
+	if (t == NULL) return;	
+	postorderTraversal(t->left);
+	postorderTraversal(t->right);
+	printf("%c  ", t->data);
+}
